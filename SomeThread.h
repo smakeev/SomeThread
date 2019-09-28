15569e797390f7a8b173628aa478f13abbd909bf
Sergey Makeev <makeev.87@gmail.com>
2019-09-28 17:22:35 +0300

    add source code files

---
+++ SomeThread.h
@@ -0,0 +1,17 @@
+//
+//  SomeThread.h
+//  SomeThread
+//
+//  Created by Sergey Makeev on 28/09/2019.
+//  Copyright © 2019 SOME projects. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SomeThread : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SomeThread.m
@@ -0,0 +1,13 @@
+//
+//  SomeThread.m
+//  SomeThread
+//
+//  Created by Sergey Makeev on 28/09/2019.
+//  Copyright © 2019 SOME projects. All rights reserved.
+//
+
+#import "SomeThread.h"
+
+@implementation SomeThread
+
+@end
